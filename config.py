72e36d8426d18899134e481b987a6dc3a7b8fd3a
justyaviz <justyaviz@gmail.com>
2026-03-03 15:19:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "").rstrip("/")
+ADMIN_TOKEN = os.getenv("ADMIN_TOKEN", "")
+PAYMENT_PROVIDER_TOKEN = os.getenv("PAYMENT_PROVIDER_TOKEN", "")
